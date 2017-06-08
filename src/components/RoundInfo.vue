bdab8b65354f6c269b77b28996da47493327ac90
Souler Ou <ou@souler.me>
2017-06-08 12:29:41 +0800

    buildup the roundinfo-page by ued ui and partial logics

---
+++ src/components/RoundInfo.vue
@@ -0,0 +1,121 @@
+<template lang='pug'>
+  .round-info
+    .round-container
+      .round-info-left
+        .sizer-left
+          img.cinemaBox(:src='movie.uri')
+        .sizer-right
+          .movie-name {{movie.name}}
+          .movie-time 时间：{{time}}
+          .movie-version 版本：2D中字
+          .movie-price 票价：￥{{price}}
+      .separate-line.pos1
+      .round-info-mid
+        .cinema-name {{cinema.name}}
+        .cinema-loc 地址：{{cinema.address}}
+        .round-position 影厅：{{position}}
+      .separate-line.pos2
+      .round-info-right
+        .total-label 总计： ￥{{total}}
+        .enter-button
+          .el-button(type='primary' , icon='el-icon-check' , @click='confirmOrder') 确认购票
+</template>
+
+<script>
+export default {
+  props: {
+    movie: Object,
+    cinema: Object,
+    total: Number,
+    time: Number,
+    price: Number,
+    position: String,
+  },
+  methods: {
+    confirmOrder: () => {
+      //TODO
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+.round-info {
+  width: 100%;
+  background-color: #ffffff;
+  height: 30%;
+}
+.round-container {
+  position:relative;
+  width: 80%;
+  top: 15%;
+  height: 100%;
+  left: 18%;
+}
+.round-info-left,.round-info-mid,.round-info-right {
+  display: inline-block;
+  position: absolute;
+  top: 13%;
+  width: 25%;
+  height: 30%;
+}
+.round-info-left .sizer-left {
+  position:relative;
+  margin-top: 0%;
+  margin-left: 0%;
+  display: inline-block;
+  width: 30%;
+  height: 100%;
+}
+.round-info-left .sizer-right {
+  position: absolute;
+  top: 0%;
+  margin-left: 5%;
+  display: inline-block;
+  width: 60%;
+  height: 100%;
+}
+.round-info-left .cinemaBox {
+  width: 100px;
+  height: 100px;
+}
+.round-info-left .movie-name {
+  font-size:20px;
+  font-weight: bold;
+}
+.round-info-mid .cinema-name {
+  font-size: 20px;
+  font-weight: bold;
+}
+
+.round-info-mid,.round-info-right {
+  height: 30%;
+  margin-left: 3%;
+}
+.round-info-right .total-label {
+  font-size: 30px;
+  font-weight: bold;
+}
+.round-info-right .enter-button {
+  margin-top: 8%;
+  margin-left: 27%;
+  position: relative;
+}
+
+.separate-line {
+  display: inline-block;
+  position: relative;
+  width: 1px;
+  background: gray;
+  height: 50%;
+}
+
+.pos1 {
+    margin-left: 23%;
+}
+.pos2 {
+    margin-left: 35%;
+}
+
+</style>
--- src/views/Main/Movies/Seats.vue
+++ src/views/Main/Movies/Seats.vue
@@ -1,16 +1,18 @@
 <template lang='pug'>
 .seats-page
+  round-info(:movie='currentMovie' , :cinema='currentCinema' , :total='amount' , :time='startTime' , :price='price' , :position='position')
   seat-selector(:seats='seats', :selecteds.sync='selecteds')
   p {{selecteds}}
 </template>
 
 <script>
 import SeatSelector from '@/components/SeatSelector'
-
+import RoundInfo from '@/components/RoundInfo'
 import { Rounds } from '@/apis/main'
 
 export default {
   components: {
+    RoundInfo,
     SeatSelector,
   },
   data () {
@@ -18,12 +20,20 @@ export default {
       seatsRows: 0,
       seatsColumns: 0,
       seats: [],
+      currentMovie: new Object(),
+      currentCinema: new Object(),
+      startTime: 0,
+      price: 0,
+      position: '',
       selecteds: [],
     }
   },
   computed: {
     roundId () {
       return this.$route.params.roundId
+    },
+    amount () {
+      return this.price * this.selecteds.length
     },
   },
   created () {
@@ -32,10 +42,15 @@ export default {
   methods: {
     fetchData () {
       Rounds.get({ rid: this.roundId })
-        .then(({ body: { seatsRows, seatsColumns, seats }}) => {
-          this.seatsRows = seatsRows
-          this.seatsColumns = seatsColumns
-          this.seats = seats
+        .then(({ body }) => {
+          this.seatsRows = body.seatsRows
+          this.seatsColumns = body.seatsColumns
+          this.seats = body.seats
+          this.price = body.price
+          this.startTime = body.beginTime
+          this.position = body.place
+          this.currentMovie = body.movie
+          this.currentCinema = body.cinema
         })
     },
   },
